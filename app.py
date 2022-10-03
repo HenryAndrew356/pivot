84c96da9fb0354cf64e92879d07058dd016d0671
Henry Andrew Medina Condo <henry.medina@ucsp.edu.pe>
2022-10-03 08:40:39 -0500

    add changes

--- app.py
+++ app.py
@@ -2,28 +2,18 @@ from flask import Flask, render_template
 
 app=Flask(__name__)
 
-person01= [
-    {
-        'fullname':'',
-        'occupation':'',
-        'perfil':'',
-        'numbertelephone':'',
-        'email':''
-    }
-]
 
-@app.route("/getData")
-def getdata():
-    name='Andrew Medina Condo'
-    return render_template ('index2.html',name=name)
+@app.route("/formcv")
+def form():
+    return render_template ('formCV.html')
+
+@app.route("/cvexample")
+def examplecv():
+    return render_template ('cvexample.html')
 
 @app.route("/returnCV")
 def cv():
-    return render_template ('index.html')
-
-@app.route("/workList")
-def listado():
-    return render_template ('index.html')
+    return render_template ('cv.html')
 
 if __name__=="__main__":
     app.run(debug=True)
