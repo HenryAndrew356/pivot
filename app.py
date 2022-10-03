c7ac9ff3ea0663dfa24ed3c612f18bf42050fdc5
Henry Andrew Medina Condo <henry.medina@ucsp.edu.pe>
2022-10-03 16:42:31 -0500

    changes

--- app.py
+++ app.py
@@ -2,19 +2,28 @@ from flask import Flask, render_template
 
 app=Flask(__name__)
 
+@app.route("/")
+def front():
+    return render_template ('frontPage.html')
 
-@app.route("/formcv")
-def form():
+@app.route("/formOFcv", methods=['POST'])
+def formcvPage():
     return render_template ('formCV.html')
 
-@app.route("/cvexample")
-def examplecv():
-    return render_template ('cvexample.html')
+@app.route("/cvexample", methods=['POST'])
+def cvclone():
+    return render_template ('cvClone.html')
 
-@app.route("/returnCV")
-def cv():
-    return render_template ('cv.html')
+@app.route("/returncv", methods=['POST'])
+def formofcv():
+    return render_template ('cvOfForm.html')
+
+@app.route("/todolist", methods=['POST'])
+def todolistF():
+    return render_template ('todo_list.html')
 
 if __name__=="__main__":
     app.run(debug=True)
+
+
 
