e7b547ff9cb1a630ba980e8519199e217f5ecbe3
Henry Andrew Medina Condo <henry.medina@ucsp.edu.pe>
2022-10-17 12:08:12 -0500

    add changes Formm CV

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask, render_template
+from flask_cors import CORS
+
 
 app=Flask(__name__)
+CORS(app)
 
-@app.route("/")
+@app.route("/",methods=['GET'])
 def front():
     return render_template ('frontPage.html')
 
@@ -22,5 +25,5 @@ def formofcv():
 def todolistF():
     return render_template ('todo_list.html')
 
-# if __name__=="__main__":
-#     app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ static/script/script.js
@@ -0,0 +1,10 @@
+// alert("This page has been loaded.")
+// datepicker=document.getElementById("datePicker")
+// datepicker.datepicker()
+
+
+// $(document).ready(function(){
+//     $('input[type=button]').click(function(){
+//         alert($('input[name=englishLevel]:checked').val()+'was selected');
+//     });
+// });
